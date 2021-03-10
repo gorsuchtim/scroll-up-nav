4cab5269dabaffb9f51ed16da8c984107cb8f606
Tim <tgorsuch@redventures.com>
2021-03-09 21:47:14 -0500

    commit initial working version of project

--- src/js/Components/NavStickyScrollUp/NavStickyScrollUp.js
+++ src/js/Components/NavStickyScrollUp/NavStickyScrollUp.js
@@ -1,5 +1,45 @@
 const NavStickyScrollUp = () => {
-  console.log("Im the freaking scrollup nav!");
+  let initialScrollTopPosition = 0;
+
+  //   Nav
+  const nav = document.querySelector("nav");
+  const navComputedStyle = window.getComputedStyle(nav);
+  const navHeight = parseInt(navComputedStyle.height);
+
+  //   Hero
+  const heroComputedStyle = window.getComputedStyle(
+    document.querySelector(".hero")
+  );
+  const heroHeight = parseInt(heroComputedStyle.height);
+
+  const handleScrollDirection = () => {
+    let direction = "";
+    const currentScrollTop =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop > initialScrollTopPosition) {
+      direction = "down";
+    } else {
+      direction = "up";
+    }
+
+    initialScrollTopPosition = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
+
+    return direction;
+  };
+
+  const handleScroll = () => {
+    const scrollDirection = handleScrollDirection();
+    if (scrollDirection === "up") {
+      nav.classList.remove("scroll--down");
+    } else {
+      if (window.pageYOffset > heroHeight - navHeight) {
+        nav.classList.add("scroll--down");
+      }
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
 };
 
 export default NavStickyScrollUp;
